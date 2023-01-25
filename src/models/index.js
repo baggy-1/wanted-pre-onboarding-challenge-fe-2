9807a55f04f798ecaa3093c2300a77acd4ca42fe
chigomuh <tngusrldnjs1@gmail.com>
2023-01-25 17:31:51 +0900

    add: models 추가
    1. model 추가
    - Todo
    - Tag

---
+++ src/models/index.js
@@ -0,0 +1,19 @@
+/**
+ * Todo 모델
+ * @typedef {{
+ * id: number,
+ * content: string,
+ * completed: boolean,
+ * category: string,
+ * tags?: Tag[],
+ * }} Todo
+ *
+ */
+
+/**
+ * Tag 모델
+ * @typedef {{
+ * id: number,
+ * name: string,
+ * }} Tag
+ */
